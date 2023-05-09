9f710bc57f292e79a76eea926b3aa2c23882c9f2
Marvin Twilley <50936642+mmtwilley@users.noreply.github.com>
2023-05-09 22:54:45 +0000

    Add a file about inputs

---
+++ BigO/Time/Inputs.java
@@ -0,0 +1,18 @@
+public class Inputs{
+
+    public static void main(Str[] args){
+        inputsPrintItems(10);
+    }
+
+    public static void inputsPrintItems(int n, int b){
+        for(int i = 0; i < n; i++){
+            System.out.println(i);
+        }
+        for(int j = 0; j < n; j++){
+            System.out.println(i);
+        }
+    }
+}
+
+// inputsPrintItem are O(a +b)
+// That's why you have to use different terms for inputs
